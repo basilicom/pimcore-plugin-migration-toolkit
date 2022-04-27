e5a64dcddf153a095c05490180f1994f365ef751
alexander.heidrich <alexander.heidrich@basilicom.de>
2022-04-27 13:41:01 +0200

    Added asset creation

--- src/Helper/AssetMigrationHelper.php
+++ src/Helper/AssetMigrationHelper.php
@@ -7,11 +7,60 @@ use Exception;
 use Pimcore\Model\Asset;
 use Pimcore\Model\Asset\Folder;
 use Pimcore\Model\Asset\Service as AssetService;
+use Pimcore\Model\Element\Service;
 
 class AssetMigrationHelper extends AbstractMigrationHelper
 {
-    // bastodo: add support for asset files
+    /**
+     * @throws Exception
+     */
+    public function createAsset(string $dataSource, string $targetFolderPath, ?string $assetKey = null): Asset
+    {
+        if (file_exists($dataSource) === false || is_readable($dataSource) === false) {
+            throw new Exception('could not find or read file for asset creation');
+        }
 
+        /** @var Asset\Folder $targetFolder */
+        $targetFolder = AssetService::createFolderByPath($targetFolderPath);
+
+        $fileinfo = pathinfo($dataSource);
+        $key = Service::getValidKey($assetKey ?? $fileinfo['basename'], 'asset');
+
+        $fullPath = $targetFolder->getFullPath() . '/' . $key;
+        if (Asset::getByPath($fullPath, true) instanceof Asset) {
+            $message = sprintf('The Asset "%s" could not be created, because already exists at "%s".', $key, $fullPath);
+            throw new InvalidSettingException($message);
+        }
+
+        $asset = new Asset();
+        $asset->setKey($key);
+        $asset->setParent($targetFolder);
+        $asset->setData(file_get_contents($dataSource));
+        $asset->save();
+
+        return $asset;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function updateAsset(Asset $asset, string $dataSource, ?string $newAssetName = null): void
+    {
+        if (file_exists($dataSource) === false || is_readable($dataSource) === false) {
+            throw new Exception('could not find or read file for asset creation');
+        }
+
+        $fileinfo = pathinfo($dataSource);
+        $key = Service::getValidKey($newAssetName ?? $fileinfo['basename'], 'asset');
+
+        $asset->setKey($key);
+        $asset->setData(file_get_contents($dataSource));
+        $asset->save();
+    }
+
+    /**
+     * @throws InvalidSettingException
+     */
     public function createFolderByParentId(string $name, int $parentId): void
     {
         $parent = Folder::getById($parentId);
@@ -21,6 +70,7 @@ class AssetMigrationHelper extends AbstractMigrationHelper
                 'The Folder "%s" could not be created, because the parent does not exist',
                 $name
             );
+
             throw new InvalidSettingException($message);
         }
 
@@ -29,19 +79,24 @@ class AssetMigrationHelper extends AbstractMigrationHelper
         $this->createFolderByPath($intendedPath);
     }
 
+    /**
+     * @throws InvalidSettingException
+     */
     public function createFolderByPath(string $path): void
     {
         try {
             AssetService::createFolderByPath($path);
         } catch (Exception $exception) {
-            $message = sprintf(
-                'The Folder "%s" could not be created.',
-                $path
-            );
+            $message = sprintf('The Folder "%s" could not be created.', $path);
+
             throw new InvalidSettingException($message);
         }
     }
 
+    /**
+     * @throws InvalidSettingException
+     * @throws Exception
+     */
     public function deleteById(int $id): void
     {
         if ($id === 1) {
@@ -53,12 +108,17 @@ class AssetMigrationHelper extends AbstractMigrationHelper
         if (empty($asset)) {
             $message = sprintf('Asset with id "%s" can not be deleted, because it does not exist.', $id);
             $this->getOutput()->writeMessage($message);
+
             return;
         }
 
         $asset->delete();
     }
 
+    /**
+     * @throws InvalidSettingException
+     * @throws Exception
+     */
     public function deleteByPath(string $path): void
     {
         if (empty($path)) {
@@ -70,6 +130,7 @@ class AssetMigrationHelper extends AbstractMigrationHelper
         if (empty($asset)) {
             $message = sprintf('Asset with path "%s" can not be deleted, because it does not exist.', $path);
             $this->getOutput()->writeMessage($message);
+
             return;
         }
 
