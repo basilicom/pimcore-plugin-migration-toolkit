875f4eb0aa97c0103087a7346aad5841570bea9a
alexander.heidrich <alexander.heidrich@basilicom.de>
2023-06-20 16:07:45 +0200

    Allow unsetting of timeout

--- src/Command/MigrateInSeparateProcessesCommand.php
+++ src/Command/MigrateInSeparateProcessesCommand.php
@@ -6,6 +6,7 @@ use Pimcore;
 use Pimcore\Console\AbstractCommand;
 use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
@@ -14,16 +15,28 @@ class MigrateInSeparateProcessesCommand extends AbstractCommand
     const LOG_EMPTY_LINE     = '                                                            ';
     const LOG_SEPARATOR_LINE = '============================================================';
 
+    private const OPTION_TIMEOUT = 'timeout';
+
     protected static $defaultName = 'basilicom:migrations:migrate-in-separate-processes';
 
     protected function configure()
     {
         $this->setDescription('Executes the same migrations as the pimcore:migrations:migrate command, '.
-            'but each one is run in a separate process, to prevent problems with PHP classes that changed during the runtime.');
+            'but each one is run in a separate process, to prevent problems with PHP classes that changed during the runtime.')
+
+            ->addOption(
+                self::OPTION_TIMEOUT,
+                't',
+                InputOption::VALUE_OPTIONAL,
+                'An optional timeout to allow execution of very huge migrations. Set "0" to disable timeout.',
+                120
+            );;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $timeout = (int) $input->getOption(self::OPTION_TIMEOUT);
+
         // The following prevents problems when the container changes during runtime - which is the case with migrations
         $eventDispatcher = Pimcore::getKernel()->getContainer()->get('event_dispatcher');
         foreach ($eventDispatcher->getListeners(ConsoleEvents::TERMINATE) as $listener) {
@@ -39,6 +52,10 @@ class MigrateInSeparateProcessesCommand extends AbstractCommand
 
         $output->writeln('Following migrations will be executed: ' . PHP_EOL . implode(PHP_EOL, $unexecutedMigrations));
 
+        if ($timeout <= 0) {
+            $output->writeln(PHP_EOL . '<comment>⚠️ Migration timeout has been disabled.</comment>' . PHP_EOL);
+        }
+
         foreach ($unexecutedMigrations as $migration) {
             $output->writeln(self::LOG_EMPTY_LINE);
             $output->writeln(self::LOG_SEPARATOR_LINE);
@@ -49,7 +66,7 @@ class MigrateInSeparateProcessesCommand extends AbstractCommand
                 ['bin/console', 'pimcore:migrations:execute', $migration, '--no-interaction'],
                 PIMCORE_PROJECT_ROOT
             );
-            $process->setTimeout(120);
+            $process->setTimeout($timeout > 0 ? $timeout : null);
             $process->run(function ($type, $buffer) use ($output) {
                 if (Process::ERR === $type) {
                     $output->writeln('<error>' . $buffer . '</error>');
